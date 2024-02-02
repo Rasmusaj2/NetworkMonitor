d7f2f313dbe13b8367187d73c392a5cf2215c678
Rasmus Jensen <github@rasmus.zip>
2024-02-02 09:34:59 +0100

    Refactor drawGraph function
    Refactored drawGraph function for improved readability

--- networkMonitor.js
+++ networkMonitor.js
@@ -47,7 +47,6 @@ function parseArgs() {
                 options.interval = parseInt(value, 10);
                 break;
             default:
-                // unknown case, print usage
                 console.log(`Unknown option: ${arg}`);
                 printUsage();
                 process.exit(1); 
@@ -55,7 +54,7 @@ function parseArgs() {
     }
 }
   
-  // console log at error for feedback
+
 function printUsage() {
     console.log(`
     Usage: node app.js [options]
@@ -76,10 +75,7 @@ function printUsage() {
 function format(bytes) {
     const decimals = 2;
     if (bytes === 0) return '0 B';
-    const size = options.size; //technicality to self.
-    // bytes are usually in 1000. 1024 is for mebibytes, gibibytes, so on, so it should be MiB, GiB, but whatever
-    //software usually does 1024 tho, especially windows for drive space, but this is network so maybe 1000 is better?
-    // NOTE TO SELF: NOW DEFAULTS TO 1000 ITS WHATEVER WE BALL
+    const size = options.size; 
     const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
     const dm = decimals < 0 ? 0 : decimals;
 
@@ -98,63 +94,65 @@ function sumArray(array) {
 
 function drawGraph(rxHis, txHis) {
     const yHeight = 12;
-    const xLength = options.seconds; 
-  
+    const xLength = options.seconds;
     rxHis = rxHis.slice(-xLength);
     txHis = txHis.slice(-xLength);
-  
-    const yMaxRx = Math.max(...rxHis) * yHeight / 10;
-    const yMaxTx = Math.max(...txHis) * yHeight / 10;
-    const yMax = yMaxRx > yMaxTx ? yMaxRx : yMaxTx;
-  
+
+    const graphArray = initializeGraphArray(yHeight, xLength);
+    fillGraphArray(graphArray, rxHis, txHis);
+    displayGraph(graphArray, yHeight, xLength);
+    console.log(` '${options.rxGraph}' Received, '${options.txGraph}' Transferred, '${options.graph}' Both`);
+}
+
+function initializeGraphArray(yHeight, xLength) {
     const graphArray = [];
-  
     for (let i = 0; i < yHeight; i++) {
-      graphArray[i] = [];
-      for (let j = 0; j < xLength; j++) {
-        graphArray[i][j] = " ";
-      }
-    }
-  
-    // *rxHis, #txHis, @both
-    for (let i = 0; i < yHeight; i++) {
-      const lineValue = yMax - ((yMax / yHeight) * (i + 1));
-  
-      for (let j = 0; j < xLength; j++) {
-        if (rxHis[j] >= lineValue && txHis[j] < lineValue) {
-          graphArray[i][j] = options.rxGraph; 
-        } else if (txHis[j] >= lineValue && rxHis[j] < lineValue) {
-          graphArray[i][j] = options.txGraph; 
-        } else if (rxHis[j] >= lineValue && txHis[j] >= lineValue) {
-          graphArray[i][j] = options.graph; 
+        graphArray[i] = [];
+        for (let j = 0; j < xLength; j++) {
+            graphArray[i][j] = " ";
         }
-      }
     }
-  
+    return graphArray;
+}
+
+function fillGraphArray(graphArray, rxHis, txHis) {
+    const yHeight = graphArray.length;
     for (let i = 0; i < yHeight; i++) {
-      const lineValueRx = yMax - ((yMax / yHeight) * (i + 1));
-      let currentLine = format(lineValueRx);
-      const length = [...currentLine].length;
-  
-      // Add padding for alignment
-      for (let k = length; k < 10; k++) {
-        currentLine += " ";
-      }
-  
-      currentLine += ` | `;
-  
-      for (let j = 0; j < xLength; j++) {
-        currentLine += graphArray[i][j];
-      }
-  
-      console.log(currentLine);
+        const lineValue = calculateLineValue(i, yHeight, Math.max(...rxHis), Math.max(...txHis));
+        for (let j = 0; j < graphArray[0].length; j++) {
+            updateGraphArrayElement(graphArray, rxHis, txHis, i, j, lineValue);
+        }
     }
-    console.log(` '${options.rxGraph}' Recieved, '${options.txGraph}' Transferred, '${options.graph}' Both`);
-  }
+}
+
+function calculateLineValue(i, yHeight, maxRx, maxTx) {
+    return maxRx > maxTx ? maxRx - ((maxRx / yHeight) * (i + 1)) : maxTx - ((maxTx / yHeight) * (i + 1));
+}
+
+function updateGraphArrayElement(graphArray, rxHis, txHis, i, j, lineValue) {
+    if (rxHis[j] >= lineValue && txHis[j] < lineValue) {
+        graphArray[i][j] = options.rxGraph;
+    } else if (txHis[j] >= lineValue && rxHis[j] < lineValue) {
+        graphArray[i][j] = options.txGraph;
+    } else if (rxHis[j] >= lineValue && txHis[j] >= lineValue) {
+        graphArray[i][j] = options.graph;
+    }
+}
+
+function displayGraph(graphArray, yHeight, xLength) {
+    for (let i = 0; i < yHeight; i++) {
+        const lineValueRx = calculateLineValue(i, yHeight, Math.max(...rxHistory), Math.max(...txHistory));
+	    
+        let currentLine = format(lineValueRx) + " | ";
+        for (let j = 0; j < xLength; j++) {
+            currentLine += graphArray[i][j];
+        }
+        console.log(currentLine);
+    }
+}
 
 
-async function consoleOutput() {
-    // find actual network generalized data
+async function mainLoop() {
     const networkStats = await sysinfo.networkStats();
     const networkInterface = networkStats[0];
     const {
@@ -168,10 +166,9 @@ async function consoleOutput() {
     rxHistory.push(rx_sec || 0);
     txHistory.push(tx_sec || 0);
 
-    // calc the stuff
     const rxAverage = sumArray(rxHistory)/rxHistory.length;
     const txAverage = sumArray(txHistory)/txHistory.length;
-    // triple dot cause u have to seperate the array apparently idk it didnt work before but does now
+	
     const rxMin = Math.min(...rxHistory);
     const rxMax = Math.max(...rxHistory);
     const txMin = Math.min(...txHistory);
@@ -188,21 +185,9 @@ async function consoleOutput() {
     console.log(`  Max: ${format(rxMax)}/s		  ${format(txMax)}/s`);
     drawGraph(rxHistory, txHistory);
 
-// deprecated if the tabbing works
-/*    console.log(`Transferred:`);
-    console.log(`  Total: ${format(tx_bytes)}`);
-    console.log(`  Running: ${format(sumArray(txHistory))}`);
-    console.log(`  Current: ${format(tx_sec || 0)}/s`);
-    console.log(`  Average: ${format(txAverage)}/s`);
-    console.log(`  Min: ${format(txMin)}/s`);
-    console.log(`  Max: ${format(txMax)}/s`); */
-    // drawGraph(txHistory, 'Transferred');
-	
-	
-    // find connection data for outgoing connections
+
     const peerStats = await sysinfo.networkConnections();
 
-    // 1 line for each or it gets unreadable
     const nonLocalConnections = peerStats.filter(connection => 
     !connection.peerAddress.startsWith('127.') && 
     !connection.peerAddress.startsWith('192.168.') && 
@@ -231,7 +216,6 @@ async function consoleOutput() {
     }
     });
 
-    // bullshit ass line of code
     const sortedConnections = [...uniqueConnections.values()].sort((a, b) => (b.tx_sec + b.rx_sec) - (a.tx_sec + a.rx_sec)).slice(0, options.maxPeers);
 
     sortedConnections.forEach(connection => {
@@ -246,11 +230,9 @@ async function consoleOutput() {
     });
 	
 		
-    // DEBUG PRINT STUFF WHATEVER
     if (options.debug) {console.log('DEBUG TESTING');}
     if (options.debug) {console.log(rxHistory);}
     if (options.debug) {console.log(txHistory);}
-//    if (options.debug) {console.log(plotSecs);} PLOTSECS NO LONGER USED
     if (options.debug) {console.log(networkInterface);}
     if (options.debug) {console.log(peerStats);}
     if (options.debug) {date = new Date(); console.log(date.getMilliseconds());}
@@ -260,7 +242,7 @@ async function consoleOutput() {
 rxHistory = [];
 txHistory = [];
 const main = () => {
-    setInterval(consoleOutput, options.interval);
+    setInterval(mainLoop, options.interval);
 };
 
 parseArgs();
